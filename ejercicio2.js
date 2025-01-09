124ab043a1bf4a63f34df0102ca2bb9c084c8eb6
lubiano83 <josepablolubiano@MacBook-Air-de-Jose.local>
2025-01-09 17:08:10 -0300

    agregando trycatch

--- ejercicio2.js
+++ ejercicio2.js
@@ -11,23 +11,35 @@ function manejoTienda(productosIniciales) {
     Array.isArray(productosIniciales) ? productosIniciales : [productosIniciales];
 
     const agregarProducto = ( nombre, precio, cantidad ) => {
-        const producto = { nombre, precio, cantidad };
-        if( !nombre || !precio || !cantidad ) return console.log("Todos los campos son necesarios");
-        if( typeof nombre !== "string" || typeof precio !== "number" || typeof cantidad !== "number" ) return console.log("El campo nombre debe ser tipo string y precio como cantidad deben ser tipo number");
-        tienda.push(producto);
+        try {
+            const producto = { nombre, precio, cantidad };
+            if( !nombre || !precio || !cantidad ) return console.log("Todos los campos son necesarios");
+            if( typeof nombre !== "string" || typeof precio !== "number" || typeof cantidad !== "number" ) return console.log("El campo nombre debe ser tipo string y precio como cantidad deben ser tipo number");
+            tienda.push(producto);
+        } catch (error) {
+            console.log(error.message);
+        }
     };
 
     const eliminarProductos = ( nombre ) => {
-        const productoEncontrado = tienda.findIndex(producto => producto.nombre === nombre);
-        if( productoEncontrado <= -1 ) return console.log("Ese producto no existe..");
-        tienda.splice(productoEncontrado, 1);
+        try {
+            const productoEncontrado = tienda.findIndex(producto => producto.nombre === nombre);
+            if( productoEncontrado <= -1 ) return console.log("Ese producto no existe..");
+            tienda.splice(productoEncontrado, 1);
+        } catch (error) {
+            console.log(error.message);
+        }
     };
 
     const valorTotal = () => {
-        const precioTotal = tienda.reduce((acc, item) => {
-            return acc + item.cantidad * item.precio
-        }, 0);
-        return console.log("Valor Total:", precioTotal);
+        try {
+            const precioTotal = tienda.reduce((acc, item) => {
+                return acc + item.cantidad * item.precio
+            }, 0);
+            return console.log("Valor Total:", precioTotal);
+        } catch (error) {
+            console.log(error.message);
+        }
     };
 
     const mostrarProductos = () => {
--- ejercicio3.js
+++ ejercicio3.js
@@ -3,30 +3,44 @@
 let contactos = [];
 
 function agregarContacto( nombre, email, telefono ) {
-    if(!nombre || !email || !telefono) return console.log("Todos los campos son requeridos");
-    if(typeof nombre !== "string" || typeof email !== "string" || typeof telefono !== "string") return console.log("Todos los campos deben ser tipo string");
-    const contacto = { nombre: nombre.toLowerCase(), email: email.toLowerCase(), telefono };
-    contactos.push(contacto);
+    try {
+        if(!nombre || !email || !telefono) return console.log("Todos los campos son requeridos");
+        if(typeof nombre !== "string" || typeof email !== "string" || typeof telefono !== "string") return console.log("Todos los campos deben ser tipo string");
+        const contacto = { nombre: nombre.toLowerCase(), email: email.toLowerCase(), telefono };
+        contactos.push(contacto);
+    } catch (error) {
+        console.log(error.message);
+    }
 };
 
 const eliminarContacto = function( email ) {
-    if( !email || typeof email !== "string" ) return console.log("El campo email es requerido y debe ser tipo string");
-    const contactoEncontrado = contactos.find(contacto => contacto.email === email);
-    if(!contactoEncontrado) return console.log("Ese contacto no existe");
-    contactos = contactos.filter(contacto => contacto.email !== email);
+    try {
+        if( !email || typeof email !== "string" ) return console.log("El campo email es requerido y debe ser tipo string");
+        const contactoEncontrado = contactos.find(contacto => contacto.email === email);
+        if(!contactoEncontrado) return console.log("Ese contacto no existe");
+        contactos = contactos.filter(contacto => contacto.email !== email);
+    } catch (error) {
+        console.log(error.message);
+    }
 };
 
 function buscarContacto( nombre ) {
-    const contactoEncontrado = contactos.find(contacto => contacto.nombre === nombre);
-    return console.log("Contacto Encontrado:", contactoEncontrado);
+    try {
+        const contactoEncontrado = contactos.find(contacto => contacto.nombre === nombre);
+        return console.log("Contacto Encontrado:", contactoEncontrado);
+    } catch (error) {
+        console.log(error.message);
+    }
 };
 
 const mostrarContactos = () => {
-    console.log("Todos los Contactos:")
-    contactos.forEach(contacto => console.log(contacto));
+    try {
+        console.log("Todos los Contactos:")
+        contactos.forEach(contacto => console.log(contacto));
+    } catch (error) {
+        console.log(error.message);
+    }
 };
-
-
 
 agregarContacto("josé pablo", "lubiano83@gmail.com", "+56988310042");
 agregarContacto("papo", "jplubiano@hotmail.com", "+56988310042");
